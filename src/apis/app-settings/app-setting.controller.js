6884b4bb770ad24899b096bdc14b08bfe79d9ff6
MangotechProjects <taimur.mangotech@gmail.com>
2022-07-06 19:33:02 +0500

    update notifications users

--- src/apis/app-settings/app-setting.controller.js
+++ src/apis/app-settings/app-setting.controller.js
@@ -1,108 +1,127 @@
-'use strict';
+"use strict";
 
-let appSettingModel = require('./app-setting.model');
-let userModel = require('../users/user.model');
-var { encryptText,comparePassword } = require('../../services/app.services');
-var { validator,getMinFromString,convertMinToHr } = require('../../util/helper');
-var { updateUserArray } = require('../../util/notification')
-var errorHandler = require('../../util/errorHandler');
+let appSettingModel = require("./app-setting.model");
+let userModel = require("../users/user.model");
+var { encryptText, comparePassword } = require("../../services/app.services");
+var {
+  validator,
+  getMinFromString,
+  convertMinToHr,
+} = require("../../util/helper");
+var errorHandler = require("../../util/errorHandler");
+const axios = require("axios");
 
 const updateSetting = async (req, res) => {
-    try {
-        // *request body validation
-        const validationRule = {
-            'email': 'required|email',
-            'countryCode': 'required',
-            'phoneNumber': 'required',
-            'dailyOpenTime': 'required',
-            'dailyTimeInterval': 'required',
-            'isNotifyEnable': 'required',
-            'timezoneOffset': 'required'
-        }
-    
-        validator(req.body, validationRule, {}, (err, status) => {
-            if (!status) {
-                return res.status(412).json({
-                    status: false, responseCode: 412,
-                    message: 'Validation failed', data: err
-                })
-            }
+  try {
+    // *request body validation
+    const validationRule = {
+      email: "required|email",
+      countryCode: "required",
+      phoneNumber: "required",
+      dailyOpenTime: "required",
+      dailyTimeInterval: "required",
+      isNotifyEnable: "required",
+      timezoneOffset: "required",
+    };
+
+    validator(req.body, validationRule, {}, (err, status) => {
+      if (!status) {
+        return res.status(412).json({
+          status: false,
+          responseCode: 412,
+          message: "Validation failed",
+          data: err,
         });
-        
-        // *extract param from request body
-        const { email, password, countryCode ,phoneNumber, dailyOpenTime, dailyTimeInterval, isNotifyEnable, timezoneOffset } = req.body;
+      }
+    });
 
-        if(password){
-            if(password.length < 9){
-                return res.status(400).json({
-                    status: false,
-                    message: "Password length must be greater than equal to 8",
-                })
-            }
-        }
-        // *encrypt incoming password
-        const hashPassword = await encryptText(password);
+    // *extract param from request body
+    const {
+      email,
+      password,
+      countryCode,
+      phoneNumber,
+      dailyOpenTime,
+      dailyTimeInterval,
+      isNotifyEnable,
+      timezoneOffset,
+    } = req.body;
 
-        let setUserModelQuery = {};
-        if (email) setUserModelQuery.email = email;
-        if (password) setUserModelQuery.password = hashPassword;
-        if (countryCode) setUserModelQuery.countryCode = countryCode;
-        if (phoneNumber) setUserModelQuery.phone_number = phoneNumber;
-
-        let setAppSettingModelQuery = {};
-        if (dailyOpenTime) {
-            // *initialize date obj
-            //const date = new Date();
-            const getTimezoneOffset = timezoneOffset;//date.getTimezoneOffset();
-            const openTimeMinutes = getMinFromString(dailyOpenTime);
-            let minutesDifference = 0;
-
-            if(getTimezoneOffset < 0) {
-                minutesDifference = openTimeMinutes - parseInt(Math.abs(getTimezoneOffset));
-            } else {
-                minutesDifference = openTimeMinutes + parseInt(Math.abs(getTimezoneOffset));
-            }
-            const setUTCOpenTime = await convertMinToHr(minutesDifference);
-            
-            setAppSettingModelQuery.dailyOpenTime = setUTCOpenTime;
-        }
-        if (dailyTimeInterval) setAppSettingModelQuery.dailyTimeInterval = dailyTimeInterval;
-        if (isNotifyEnable) setAppSettingModelQuery.isNotifyEnable = isNotifyEnable;
-
-        // *update user
-        const updateUser = await userModel.findOneAndUpdate(
-            { _id: req.user._id }, 
-            { $set: setUserModelQuery }
-        )
-        
-        // *update user settings
-        const updateAppSetting = await appSettingModel.findOneAndUpdate(
-            { user_id: req.user._id }, 
-            { $set: setAppSettingModelQuery }
-        )
-
-        if(!updateUser || !updateAppSetting){
-            return res.status(500).json({ 
-                status: false,
-                message: "Unexpected error" 
-            });
-        } else {
-            await updateUserArray();
-            return res.status(200).json({
-                status: true,
-                message: "Settings Update Successfully",
-            })
-        }
-
-    } catch (err) {
-        let error = errorHandler.handle(err)
-        return res.status(500).json({
-            status: false,
-            message: error
-        })
+    if (password) {
+      if (password.length < 9) {
+        return res.status(400).json({
+          status: false,
+          message: "Password length must be greater than equal to 8",
+        });
+      }
     }
-}
+    // *encrypt incoming password
+    const hashPassword = await encryptText(password);
+
+    let setUserModelQuery = {};
+    if (email) setUserModelQuery.email = email;
+    if (password) setUserModelQuery.password = hashPassword;
+    if (countryCode) setUserModelQuery.countryCode = countryCode;
+    if (phoneNumber) setUserModelQuery.phone_number = phoneNumber;
+
+    let setAppSettingModelQuery = {};
+    if (dailyOpenTime) {
+      // *initialize date obj
+      //const date = new Date();
+      const getTimezoneOffset = timezoneOffset; //date.getTimezoneOffset();
+      const openTimeMinutes = getMinFromString(dailyOpenTime);
+      let minutesDifference = 0;
+
+      if (getTimezoneOffset < 0) {
+        minutesDifference =
+          openTimeMinutes - parseInt(Math.abs(getTimezoneOffset));
+      } else {
+        minutesDifference =
+          openTimeMinutes + parseInt(Math.abs(getTimezoneOffset));
+      }
+      const setUTCOpenTime = await convertMinToHr(minutesDifference);
+
+      setAppSettingModelQuery.dailyOpenTime = setUTCOpenTime;
+    }
+    if (dailyTimeInterval)
+      setAppSettingModelQuery.dailyTimeInterval = dailyTimeInterval;
+    if (isNotifyEnable) setAppSettingModelQuery.isNotifyEnable = isNotifyEnable;
+
+    // *update user
+    const updateUser = await userModel.findOneAndUpdate(
+      { _id: req.user._id },
+      { $set: setUserModelQuery }
+    );
+
+    // *update user settings
+    const updateAppSetting = await appSettingModel.findOneAndUpdate(
+      { user_id: req.user._id },
+      { $set: setAppSettingModelQuery }
+    );
+
+    if (!updateUser || !updateAppSetting) {
+      return res.status(500).json({
+        status: false,
+        message: "Unexpected error",
+      });
+    } else {
+      //let localURL = "http://localhost:3001/notifications/api/update/users";
+      let liveURL = "http://app.zenoteit.com/notifications/api/update/users";
+      const resp = await axios.get(liveURL);
+      return res.status(200).json({
+        status: true,
+        message: "Settings Update Successfully",
+      });
+    }
+  } catch (err) {
+    let error = errorHandler.handle(err);
+    return res.status(500).json({
+      status: false,
+      message: error,
+    });
+  }
+};
 
 module.exports = {
-    updateSetting: updateSetting,
-}
+  updateSetting: updateSetting,
+};
--- src/apis/notifications/notification.controller.js
+++ src/apis/notifications/notification.controller.js
@@ -1,185 +1,198 @@
-'use strict';
+"use strict";
 
-let notificationTypeModel = require('./notification-type.model');
-let userNotificationModel = require('./user-notifications.model');
-let userNotificationLogModel = require('../notification-log/user-notification-log.model');
-var { updateSocketUserNotificationArray } = require('../../util/notification')
-var { validator } = require('../../util/helper');
-var errorHandler = require('../../util/errorHandler');
+let notificationTypeModel = require("./notification-type.model");
+let userNotificationModel = require("./user-notifications.model");
+let userNotificationLogModel = require("../notification-log/user-notification-log.model");
+var { validator } = require("../../util/helper");
+var errorHandler = require("../../util/errorHandler");
+const axios = require("axios");
 
 const addNotificationType = async (req, res) => {
-    try {
-        // *request body validation
-        const validationRule = {
-            'title': 'required|string',
-        }
-    
-        validator(req.body, validationRule, {}, (err, status) => {
-            if (!status) {
-                return res.status(412).json({
-                    status: false, responseCode: 412,
-                    message: 'Validation failed', data: err
-                })
-            }
+  try {
+    // *request body validation
+    const validationRule = {
+      title: "required|string",
+    };
+
+    validator(req.body, validationRule, {}, (err, status) => {
+      if (!status) {
+        return res.status(412).json({
+          status: false,
+          responseCode: 412,
+          message: "Validation failed",
+          data: err,
         });
-        
-        // *create obj for db insert
-        let obj = {
-            title: req.body.title,
-        };
+      }
+    });
 
-        // *insert
-        const notificationType = new notificationTypeModel(obj); 
-        await notificationType.save();
+    // *create obj for db insert
+    let obj = {
+      title: req.body.title,
+    };
 
-        res.status(200).json({
-            status: true,
-            message: "Notification Type Added Successfully",
-        })
-        
-    } catch (err) {
-        let error = errorHandler.handle(err)
-        return res.status(500).json({
-            status: false,
-            message: error
-        })
-    }
-}
+    // *insert
+    const notificationType = new notificationTypeModel(obj);
+    await notificationType.save();
+
+    res.status(200).json({
+      status: true,
+      message: "Notification Type Added Successfully",
+    });
+  } catch (err) {
+    let error = errorHandler.handle(err);
+    return res.status(500).json({
+      status: false,
+      message: error,
+    });
+  }
+};
 
 const addUserNotification = async (req, res) => {
-    try {
-        // *request body validation
-        const validationRule = {
-            'type_id': 'required',
-            'notification': 'required|string',
-        }
-    
-        validator(req.body, validationRule, {}, (err, status) => {
-            if (!status) {
-                return res.status(412).json({
-                    status: false, responseCode: 412,
-                    message: 'Validation failed', data: err
-                })
-            }
+  try {
+    // *request body validation
+    const validationRule = {
+      type_id: "required",
+      notification: "required|string",
+    };
+
+    validator(req.body, validationRule, {}, (err, status) => {
+      if (!status) {
+        return res.status(412).json({
+          status: false,
+          responseCode: 412,
+          message: "Validation failed",
+          data: err,
         });
+      }
+    });
 
-        // *get type title by type_id
-        let result = await notificationTypeModel.findOne({
-            _id: req.body.type_id
-        });
+    // *get type title by type_id
+    let result = await notificationTypeModel.findOne({
+      _id: req.body.type_id,
+    });
 
-        // *get last order number
-        let orderNo = await userNotificationModel.find({}).limit(1).sort({order_no: -1})
+    // *get last order number
+    let orderNo = await userNotificationModel
+      .find({})
+      .limit(1)
+      .sort({ order_no: -1 });
 
-        if(!orderNo){
-            orderNo = 1;
-        } else {
-            orderNo = +orderNo[0].order_no + 1;
-        }
-        
-        // *create obj for db insert
-        let obj = {
-            type_id: req.body.type_id,
-            type_title: result.title,
-            notification: req.body.notification,
-            order_no: orderNo,
-        };
-
-        // *insert
-        const userNotification = new userNotificationModel(obj); 
-        await userNotification.save();
-
-        res.status(200).json({
-            status: true,
-            message: "User Notification Added Successfully",
-        })
-        
-    } catch (err) {
-        let error = errorHandler.handle(err)
-        return res.status(500).json({
-            status: false,
-            message: error
-        })
+    if (!orderNo) {
+      orderNo = 1;
+    } else {
+      orderNo = +orderNo[0].order_no + 1;
     }
-}
+
+    // *create obj for db insert
+    let obj = {
+      type_id: req.body.type_id,
+      type_title: result.title,
+      notification: req.body.notification,
+      order_no: orderNo,
+    };
+
+    // *insert
+    const userNotification = new userNotificationModel(obj);
+    await userNotification.save();
+
+    res.status(200).json({
+      status: true,
+      message: "User Notification Added Successfully",
+    });
+  } catch (err) {
+    let error = errorHandler.handle(err);
+    return res.status(500).json({
+      status: false,
+      message: error,
+    });
+  }
+};
 
 const getUserWiseNotifications = async (req, res) => {
-    try {
-        let notifications = [];
+  try {
+    let notifications = [];
 
-        // *set data object
-        let d = new Date();
-        let date = d.getFullYear() + '-' + (+d.getMonth() + 1) + '-' + d.getDate();
+    // *set data object
+    let d = new Date();
+    let date = d.getFullYear() + "-" + (+d.getMonth() + 1) + "-" + d.getDate();
 
-        let result = await userNotificationLogModel.aggregate([
-            { $match: { $and:[ {"user_id":req.user._id,'creationAt':{$gte:new Date(date)}} ] }  },
-            {
-                $lookup: {
-                    from: 'user_notifications',
-                    localField: 'notification_id',
-                    foreignField: '_id',
-                    as: 'notifications',
-                },
-            },
-            {
-                $project: {
-                    user_id: false,
-                    notification_id: false,
-                    notification_order_no: false,
-                    updatedAt: false,
-                    __v: false,
-                    "notifications._id": false,
-                    "notifications.type_id": false,
-                    "notifications.creationAt": false,
-                    "notifications.updatedAt": false,
-                    "notifications.__v": false, 
-                }
-            }
-        ])
+    let result = await userNotificationLogModel.aggregate([
+      {
+        $match: {
+          $and: [
+            { user_id: req.user._id, creationAt: { $gte: new Date(date) } },
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: "user_notifications",
+          localField: "notification_id",
+          foreignField: "_id",
+          as: "notifications",
+        },
+      },
+      {
+        $project: {
+          user_id: false,
+          notification_id: false,
+          notification_order_no: false,
+          updatedAt: false,
+          __v: false,
+          "notifications._id": false,
+          "notifications.type_id": false,
+          "notifications.creationAt": false,
+          "notifications.updatedAt": false,
+          "notifications.__v": false,
+        },
+      },
+    ]);
 
-        result.map(function(val, index){
-            let time = JSON.stringify(val.creationAt);
-            notifications.push({
-                _id: val._id,
-                isRead: val.isRead,
-                notificationTitle: val.notifications[0].type_title,
-                notificationDescription: val.notifications[0].notification,
-                notificationOrder: val.notifications[0].order_no,
-                notificationTime: time.substr(12,5)
-            })
-        })
+    result.map(function (val, index) {
+      let time = JSON.stringify(val.creationAt);
+      notifications.push({
+        _id: val._id,
+        isRead: val.isRead,
+        notificationTitle: val.notifications[0].type_title,
+        notificationDescription: val.notifications[0].notification,
+        notificationOrder: val.notifications[0].order_no,
+        notificationTime: time.substr(12, 5),
+      });
+    });
 
-        // *update read notification bit to true
-        let userNotificationLogObj = {
-            isRead: true,
-        }
+    // *update read notification bit to true
+    let userNotificationLogObj = {
+      isRead: true,
+    };
 
-        const updateUserNotificationLogModel = await userNotificationLogModel.updateMany(
-            { user_id: req.user._id,isRead: false }, 
-            { $set: userNotificationLogObj }
-        )
-        
-        if(updateUserNotificationLogModel){
-            await updateSocketUserNotificationArray(date);
-        }
+    const updateUserNotificationLogModel =
+      await userNotificationLogModel.updateMany(
+        { user_id: req.user._id, isRead: false },
+        { $set: userNotificationLogObj }
+      );
 
-        return res.status(200).json({
-            status: true,
-            message: 'Notifications',
-            data: notifications
-        })
-
-    } catch (err) {
-        let error = errorHandler.handle(err)
-        return res.status(500).json({
-            status: false,
-            message: error
-        })
+    if (updateUserNotificationLogModel) {
+      //let localURL = "http://localhost:3001/notifications/api/update/notifications";
+      let liveURL = "http://app.zenoteit.com/notifications/api/update/notifications";
+      const resp = await axios.get(liveURL);
     }
-}
+
+    return res.status(200).json({
+      status: true,
+      message: "Notifications",
+      data: notifications,
+    });
+  } catch (err) {
+    let error = errorHandler.handle(err);
+    return res.status(500).json({
+      status: false,
+      message: error,
+    });
+  }
+};
 
 module.exports = {
-    addNotificationType: addNotificationType,
-    addUserNotification: addUserNotification,
-    getUserWiseNotifications: getUserWiseNotifications
-}
+  addNotificationType: addNotificationType,
+  addUserNotification: addUserNotification,
+  getUserWiseNotifications: getUserWiseNotifications,
+};
