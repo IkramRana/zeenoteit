e154d0658533a8b68835d1aacf3afa3da444a2dd
Airas Khan <airas.mangotech@gmail.com>
2022-10-17 15:24:13 +0500

    port FIx

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ mongoose.connect(process.env.DB_URL, {
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-var server = app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.set('port', (process.env.PORT || 5000));
+// var server = app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 // var server = require('http').createServer(app);
 
 // *init cron job
